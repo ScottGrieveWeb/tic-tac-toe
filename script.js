8816a3c70fa8d05d4c037005e11721bf4301eab6
ScottGrieveWeb <scottgrieveweb@gmail.com>
2024-11-14 21:41:48 +0000

    Created initial game board function factory

---
+++ script.js
@@ -0,0 +1,23 @@
+const gameBoard = (function () {
+    const rows = 3;
+    const columns = 3;
+    const board = [];
+
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; j < columns; j++) {
+            board[i].push(Cell());
+        }
+    }
+    return {
+        board: board
+     };
+})();
+
+function Cell() {
+    let value = 0;
+
+    return value;
+}
+
+console.log(gameBoard.board);
